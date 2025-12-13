9861ddd5dcccf0188b9c73ac33e42e31cd13b684
Andrii Maltsev <maltsand@fit.cvut.cz>
2025-12-13 07:23:56 +0100

    A lot of stuff

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styles from "./Footer.module.scss";
+import { ReactComponent as LogoSvg } from "../../Images/Icons/logo.svg";
+import { Container } from "../Container/Container";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <Container>
+          <div className={styles["footer__content"]}>
+            <LogoSvg className={styles["footer__logo"]} />
+
+            <div>
+              <h3 className={styles["footer__title"]}>Address</h3>
+              <address className={styles["footer__address-text"]}>
+                <p>Svobody str. 35</p>
+                <p>Kyiv</p>
+                <p>Ukraine</p>
+              </address>
+            </div>
+
+            <div>
+              <h3 className={styles["footer__title"]}>Contact us</h3>
+              <div className={styles["footer__socials"]}>
+                <a
+                  href="https://www.instagram.com/"
+                  className={styles["footer__social-link"]}
+                  target="blank"
+                >
+                </a>
+                <a
+                  href="https://www.facebook.com/"
+                  className={styles["footer__social-link"]}
+                  target="blank"
+                >
+                </a>
+                <a
+                  href="https://www.whatsapp.com/"
+                  className={styles["footer__social-link"]}
+                  target="blank"
+                >
+                </a>
+              </div>
+            </div>
+          </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,96 @@
+import {useState} from "react";
+import styles from "./Hero.module.scss";
+import { Container } from "../Container/Container";
+import { ReactComponent as SearchSvg } from "../../Images/Icons/search.svg";
+
+const Hero = ({ onSearch }) => { 
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSearchClick = () => {
+    if (inputValue.trim()) {
+      onSearch(inputValue); 
+      setInputValue(""); 
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') handleSearchClick();
+  };
+
+  const today = new Date();
+
+  const monthYear = today.toLocaleDateString("en-US", {
+    month: "long",
+    year: "numeric",
+  });
+
+  const dayName = today.toLocaleDateString("en-US", {
+    weekday: "long",
+  });
+
+  const getDayWithSuffix = (date) => {
+    const day = date.getDate();
+    if (day > 3 && day < 21) return `${day}th`;
+    switch (day % 10) {
+      case 1:
+        return `${day}st`;
+      case 2:
+        return `${day}nd`;
+      case 3:
+        return `${day}rd`;
+      default:
+        return `${day}th`;
+    }
+  };
+
+  const fullDayString = `${dayName}, ${getDayWithSuffix(today)}`;
+
+  return (
+    <div className={styles.hero}>
+      <Container>
+        <div className={styles["hero__container"]}>
+          <h1 className={styles["hero__title"]}>Weather dashboard</h1>
+
+          <div className={styles["hero__info"]}>
+            <div
+              className={`${styles["hero__text-block"]} ${styles["hero__text-block--left"]}`}
+            >
+              <p>
+                Create your personal list of favorite cities and always be aware
+                of the weather.
+              </p>
+            </div>
+
+            <div className={styles["hero__separator"]}></div>
+
+            <div
+              className={`${styles["hero__text-block"]}`}
+            >
+              <p>{monthYear}</p>
+              <p>{fullDayString}</p>
+            </div>
+          </div>
+
+         <div className={styles['hero__search']}>
+          <input 
+            type="text" 
+            placeholder="Search location..." 
+            className={styles['hero__search-input']}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <button 
+            className={styles['hero__search-btn']} 
+            onClick={handleSearchClick}
+          >
+            <SearchSvg/>
+          </button>
+      </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
